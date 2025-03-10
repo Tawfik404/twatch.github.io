e2f41652268376adb98c89a4cf34c50e895acf72
Tawfik404 <tawfikabouhmad.404@gmail.com>
2025-03-10 17:43:53 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function result(response){
     poster.innerHTML = "<img src="+response["Poster"]+"alt='poster' id='imgmovie'>";
     plot.innerHTML = `<h3><strong id="infotitle">Plot : </strong>${response["Plot"]}</h3>`;
     release.innerHTML = `<h3><strong  id="infotitle">Release date : </strong>${response["Released"]}</h3>`;
-    genre.innerHTML = `<h3><stron id="infotitle">Genre : </strong>${response["Genre"]}</h3>`;
+    genre.innerHTML = `<h3><strong id="infotitle">Genre : </strong>${response["Genre"]}</h3>`;
     rate.innerHTML = `<h3><strong id="infotitle">Rate : </strong>${response["imdbRating"]}/10
         <svg id="star" width="25px" height="25px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <path d="M16,6.204l-5.528-0.803L8,0.392L5.528,5.401L0,6.204l4,3.899l-0.944,5.505L8,13.009l4.944,2.599L12,10.103L16,6.204z" fill="#F7BB07"/>
@@ -68,6 +68,7 @@ function err(){
       //cat.setAttribute("width", "100");
     //   cat.style.height = "100px";
       cat.style.width = "200px";
+      cat.style.marginTop = "20vh";
     });
     error.innerHTML = "<h1 id='error'>Not found, try again:(</h1>";
     error.style.height = "20vh";
